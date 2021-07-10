bd55620253371bba48b4a04debcf5e9334431d9c
FabioS <52416951+fabio2100@users.noreply.github.com>
2021-07-09 22:05:12 -0300

    Update conexion.php

--- models/conexion.php
+++ models/conexion.php
@@ -2,7 +2,7 @@
   class Conectar{
     public static function conexion(){
       try {
-        $conexion = new PDO('mysql:host=localhost;dbname=cursosqldb','root','');
+        $conexion = new PDO('mysql:host=*******;dbname=*******','*******','********');
         $conexion -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         $conexion -> exec("SET CHARACTER SET UTF8");
       } catch (Exception $e) {
